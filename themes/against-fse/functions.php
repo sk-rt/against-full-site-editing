e1b06c937e9da8f7407b2497795e2e21e4db4eea
Ryuta Sakai <info@sakairyuta.com>
2021-10-27 16:45:04 +0900

    feat: remove parent theme setting

--- themes/against-fse/functions.php
+++ themes/against-fse/functions.php
@@ -1,24 +1,27 @@
 <?php
 
+/**
+ * remove parent theme setting
+ */
+function remove_parent_theme_action()
+{
+    remove_action('init', 'twenty_twenty_one_register_block_styles');
+    remove_action('enqueue_block_editor_assets', 'twentytwentyone_block_editor_script');
+    remove_action('after_setup_theme', 'twenty_twenty_one_setup');
+}
+add_action('after_setup_theme', 'remove_parent_theme_action', 9);
 
+/**
+ * utility
+ */
+require get_stylesheet_directory() .'/include/utility.php';
 
-/***************************************************************
+/**
+ * setup post support
+ */
+require get_stylesheet_directory() .'/include/post.php';
 
-utility
-
- ***************************************************************/
-get_template_part('include/utility');
-
-/***************************************************************
-
-post support
-
- ***************************************************************/
-get_template_part('include/post');
-
-/***************************************************************
-
-block editor
-
- ***************************************************************/
-get_template_part('include/editor');
+/**
+ * block editor
+ */
+require get_stylesheet_directory() .'/include/editor.php';
